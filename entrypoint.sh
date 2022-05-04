4baa00b708196a069d75ad553eea48eea878f7c9
François Monteil <francois.monteil@protonmail.com>
2022-05-04 10:34:50 +0200

    fix: get kubectl version from env

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,15 @@ set -e
 echo "$KUBE_CONFIG_DATA" | base64 -d > /tmp/config
 export KUBECONFIG=/tmp/config
 
+if [ -z ${KUBECTL_VERSION+x} ] ; then
+    echo "kubectl version: $(kubectl version --client --short)"
+else
+    echo "Repulling kubectl for version $KUBECTL_VERSION"
+    rm /usr/bin/kubectl
+    curl -LO https://storage.googleapis.com/kubernetes-release/release/$KUBECTL_VERSION/bin/linux/amd64/kubectl && \
+        chmod +x kubectl && \
+        mv kubectl /usr/local/bin/
+    echo "kubectl version: $(kubectl version --client --short)"
+fi
+
 sh -c "kubectl $*"
